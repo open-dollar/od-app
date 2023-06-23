0a753fce26732098bed04d617d09aea40aa0bf6a
Ardy <116569704+0xArdy@users.noreply.github.com>
2023-06-23 07:26:34 -0400

    fix: build (#40)

--- craco.config.js
+++ craco.config.js
@@ -1,22 +1,32 @@
+const cracoAlias = require('craco-alias')
+
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ['./src/**/*.{html,js}'],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [
+        {
+            plugin: cracoAlias,
+            options: {
+                source: 'tsconfig',
+                baseUrl: '.',
+                tsConfigPath: './tsconfig.json',
+            },
+        },
+    ],
     webpack: {
         configure: {
             resolve: {
                 fallback: {
-                    "util": false,
-                    "assert": false,
-                    "http": false,
-                    "https": false,
-                    "os": false,
-                    "stream": false
-                }
-            }
-        }
-    }
-
+                    util: false,
+                    assert: false,
+                    http: false,
+                    https: false,
+                    os: false,
+                    stream: false,
+                },
+            },
+        },
+    },
 }

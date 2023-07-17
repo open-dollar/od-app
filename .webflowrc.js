a298c63497dcf49d828124b4d1fe0a79114d1d54
alekseevaiana <alekseevadev@gmail.com>
2023-07-17 10:40:28 -0700

    add webflow config file

---
+++ .webflowrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: "https://api.webflow.com",
+    rootDir: "./devlink",
+    siteId: "64ac4f0e4fd899bd9c0009aa",
+    authToken: process.env.WF_AUTH_TOKEN, // An environment variable is recommended for this field.
+    cssModules: true,
+};

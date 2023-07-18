c9dd9520c7298ba0a9474f9f91ee323c3966cc21
jahabeebs <jahgoreds@gmail.com>
2023-07-18 15:30:16 -0400

    fix ts-ignore issues

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -6,9 +6,14 @@ declare module 'jazzicon' {
     export default function (diameter: number, seed: number): HTMLElement
 }
 declare module 'comma-number'
+type ExtensionForProvider = {
+    on: (event: string, callback: (...params: any) => void) => void
+    removeListener: (event: string, callback: (...params: any) => void) => void
+}
+type EthersProvider = ExternalProvider & ExtensionForProvider
 declare global {
     interface Window {
-        ethereum?: ExternalProvider
+        ethereum?: ExternalProvider & ExtensionForProvider
         web3?: {}
     }
 }

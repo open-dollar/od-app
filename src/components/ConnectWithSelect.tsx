902d60d8b0e4f72957ea01801c69cb8188ef9894
jahabeebs <jahgoreds@gmail.com>
2023-08-17 13:36:12 -0500

    fix connectwithselect errors

--- src/components/ConnectWithSelect.tsx
+++ src/components/ConnectWithSelect.tsx
@@ -1,18 +1,14 @@
-// @ts-nocheck
-
-
 import type { Web3ReactHooks } from '@web3-react/core'
 import {MetaMask} from "@web3-react/metamask";
 import { Network } from '@web3-react/network'
 import { WalletConnect } from '@web3-react/walletconnect'
 import { WalletConnect as WalletConnectV2 } from '@web3-react/walletconnect-v2'
-import { useCallback, useEffect, useState } from 'react'
+import { useState } from 'react'
 
-import { getAddChainParameters } from '../chains'
 import {IconWrapper} from "~/components/ConnectedWalletIcon";
 import {CoinbaseWallet} from "@web3-react/coinbase-wallet";
 
-function getStatusIcon(connector) {
+function getStatusIcon(connector: MetaMask | WalletConnect | WalletConnectV2 | CoinbaseWallet | Network) {
   if (connector instanceof MetaMask) {
     return (
         <IconWrapper size={32}>
@@ -67,28 +63,26 @@ export function ConnectWithSelect({
         <div style={{ marginBottom: '1rem' }} />
         {isActive ? (
             error ? (
-                <button>Try again?</button>
+                <div>Try again?</div>
             ) : (
-                <button
+                <div
                     onClick={() => {
                       if (connector?.deactivate) {
                         void connector.deactivate();
                       } else {
                         void connector.resetState();
                       }
-                      setDesiredChainId(undefined);
+                      setDesiredChainId(-1);
                     }}
                 >
                   Disconnect
-                </button>
+                </div>
             )
         ) : (
-            <button
-                onClick={() => switchChain(desiredChainId)}
-                disabled={isActivating || !desiredChainId}
+            <div
             >
               {error ? 'Try again?' : getStatusIcon(connector)}
-            </button>
+            </div>
         )}
       </div>
   );

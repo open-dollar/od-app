d24f9a402b93910078ff6604c2c8cf28787aca3b
jahabeebs <jahgoreds@gmail.com>
2023-08-22 11:47:06 -0500

    just change wallet

--- src/components/ConnectedWalletInfo.tsx
+++ src/components/ConnectedWalletInfo.tsx
@@ -81,11 +81,7 @@ const ConnectedWalletInfo = () => {
             <DataContainer>
                 <Connection>
                     {t('connected_with')} {connector ? formatConnectorName() : 'N/A'}
-                    {!(connector instanceof MetaMask) && !(connector instanceof CoinbaseWallet) ? (
-                        <Button text={'disconnect'} onClick={handleDisconnect} />
-                    ) : (
                         <Button text={'change'} onClick={handleChange} />
-                    )}
                 </Connection>
 
                 <Address id="web3-account-identifier-row">

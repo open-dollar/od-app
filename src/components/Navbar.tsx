3f1dbcee6ee27ce63dc0ad45dbb20d72032df07c
Patrick Gallagher <blockchainbuddha@gmail.com>
2023-09-15 16:23:33 -0700

    mentions of OD (#115)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -116,7 +116,7 @@ const Navbar = () => {
 
     const hasPendingTransactions = !!pending.length
 
-    const haiBalance = useMemo(() => {
+    const odBalance = useMemo(() => {
         const balances = connectWalletModel.tokensFetchedData
         return formatNumber(balances.OD ? utils.formatEther(balances.OD.balanceE18) : '0', 2)
     }, [connectWalletModel.tokensFetchedData])
@@ -210,7 +210,7 @@ const Navbar = () => {
                                 width={'16px'}
                                 height={'16px'}
                             />
-                            {haiBalance + ' '} OD
+                            {odBalance + ' '} OD
                             <ArrowWrapper>
                                 <ArrowDown fill={isTokenPopupVisible ? '#1499DA' : '#00587E'} />
                             </ArrowWrapper>

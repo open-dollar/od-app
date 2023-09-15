11e944a5e8d647053f0519ceb03335549601f7f5
jahabeebs <jahgoreds@gmail.com>
2023-09-15 14:58:38 -0500

    fix footer, token dropdown in nav

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import Logo from '../assets/od-full-logo-grey.svg'
 import { useStoreActions } from '~/store'
 import { Link as RouterLink } from 'react-router-dom'
-import { useHistory } from 'react-router-dom'
+import FooterBackgroundImage from '~/assets/footer-bg-art.svg'
 
 const Footer: React.FC = () => {
     const { popupsModel: popupsActions } = useStoreActions((state) => state)
@@ -19,86 +19,116 @@ const Footer: React.FC = () => {
         }
     }
     return (
-        <FooterContainer>
-            <Row className="logoRow">
-                <Column>
-                    <LogoContainer>
-                        <a href={'/'}>
-                            <img src={Logo} height={'13px'} width={'89px'} alt="OD" />
-                        </a>
-                    </LogoContainer>
-                </Column>
-            </Row>
-            <Row className="linksRow">
-                <Column>
-                    <Link target="_blank" href="https://opendollar.com/blog">
-                        Blog
-                    </Link>
-                    <Link target="_blank" href="https://opendollar.com/lite-paper">
-                        Lite Paper
-                    </Link>
-                </Column>
-                <Column>
-                    <InnerLink
-                        to="/vaults"
-                        onClick={(e) => handleLinkClick(e, false)}
-                        className={location.pathname.startsWith('/vaults') ? 'activeLink' : ''}
-                    >
-                        App
-                    </InnerLink>
-                    <InnerLink
-                        to="/auctions"
-                        onClick={(e) => handleLinkClick(e, false)}
-                        className={location.pathname.startsWith('/auctions') ? 'activeLink' : ''}
-                    >
-                        Auctions
-                    </InnerLink>
-                    <InnerLink
-                        to="/stats"
-                        onClick={(e) => handleLinkClick(e, false)}
-                        className={location.pathname.startsWith('/stats') ? 'activeLink' : ''}
-                    >
-                        Stats
-                    </InnerLink>
-                </Column>
-                <Column>
-                    <Link target="_blank" href="https://discord.opendollar.com">
-                        Discord
-                    </Link>
-                    <Link target="_blank" href="https://twitter.com/open_dollar">
-                        Twitter
-                    </Link>
-                </Column>
-            </Row>
-            <Row className="privacyRow">
-                <Column>
-                    <SmallerLink
-                        target="_blank"
-                        href="https://www.usekeyp.com/terms-of-use-privacy-policy#Privacy-Policy"
-                    >
-                        Privacy Policy
-                    </SmallerLink>
-                    <SmallerLink
-                        target="_blank"
-                        href="https://www.usekeyp.com/terms-of-use-privacy-policy#Terms-of-Use"
-                    >
-                        Terms of Service
-                    </SmallerLink>
-                </Column>
-            </Row>
-        </FooterContainer>
+        <FooterAndImageContainer>
+            <FooterContainer>
+                <Row className="logoRow">
+                    <Column>
+                        <LogoContainer>
+                            <a href={'/'}>
+                                <img src={Logo} height={'13px'} width={'89px'} alt="OD" />
+                            </a>
+                        </LogoContainer>
+                    </Column>
+                </Row>
+                <Row className="linksRow">
+                    <Column>
+                        <Link target="_blank" href="https://opendollar.com/blog">
+                            Blog
+                        </Link>
+                        <Link target="_blank" href="https://opendollar.com/lite-paper">
+                            Lite Paper
+                        </Link>
+                    </Column>
+                    <Column>
+                        <InnerLink
+                            to="/vaults"
+                            onClick={(e) => handleLinkClick(e, false)}
+                            className={location.pathname.startsWith('/vaults') ? 'activeLink' : ''}
+                        >
+                            App
+                        </InnerLink>
+                        <InnerLink
+                            to="/auctions"
+                            onClick={(e) => handleLinkClick(e, false)}
+                            className={location.pathname.startsWith('/auctions') ? 'activeLink' : ''}
+                        >
+                            Auctions
+                        </InnerLink>
+                        <InnerLink
+                            to="/stats"
+                            onClick={(e) => handleLinkClick(e, false)}
+                            className={location.pathname.startsWith('/stats') ? 'activeLink' : ''}
+                        >
+                            Stats
+                        </InnerLink>
+                    </Column>
+                    <Column>
+                        <Link target="_blank" href="https://discord.opendollar.com">
+                            Discord
+                        </Link>
+                        <Link target="_blank" href="https://twitter.com/open_dollar">
+                            Twitter
+                        </Link>
+                    </Column>
+                </Row>
+                <Row className="privacyRow">
+                    <Column>
+                        <SmallerLink
+                            target="_blank"
+                            href="https://www.usekeyp.com/terms-of-use-privacy-policy#Privacy-Policy"
+                        >
+                            Privacy Policy
+                        </SmallerLink>
+                        <SmallerLink
+                            target="_blank"
+                            href="https://www.usekeyp.com/terms-of-use-privacy-policy#Terms-of-Use"
+                        >
+                            Terms of Service
+                        </SmallerLink>
+                    </Column>
+                </Row>
+            </FooterContainer>
+            <FooterImage>
+                <img src={FooterBackgroundImage} alt="" />
+            </FooterImage>
+        </FooterAndImageContainer>
     )
 }
 
 export default Footer
 
+const FooterAndImageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+`
+
+const FooterImage = styled.div`
+    //position: fixed;
+    //bottom: 0;
+    //left: 0;
+    display: flex;
+    width: 100%;
+    z-index: -1;
+    img {
+        width: 100%;
+    }
+`
+
 const FooterContainer = styled.div`
     display: flex;
+    padding-top: 80px;
+    //position: fixed;
+    bottom: 170px;
+    width: 100%;
     flex-direction: row;
     justify-content: space-around;
     color: ${(props) => props.theme.colors.blueish};
 
     @media (max-width: 767px) {
+        bottom: 117px;
         flex-direction: column;
         justify-content: center;
     }
@@ -132,7 +162,13 @@ const Column = styled.div`
     display: flex;
     flex-direction: column;
     flex-wrap: wrap;
-    margin: 0 20px;
+    padding-left: 10px;
+    padding-right: 10px;
+
+    @media (max-width: 767px) {
+        padding-left: 20px;
+        padding-right: 20px;
+    }
 `
 
 const LogoContainer = styled.div`

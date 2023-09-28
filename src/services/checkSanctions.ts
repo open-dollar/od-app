cc9c709f0e25d19d7bc7e1b252b3bcd4ff002a99
jahabeebs <jahgoreds@gmail.com>
2023-09-28 12:25:33 -0500

    temporarily replace with bots cors changes

--- src/services/checkSanctions.ts
+++ src/services/checkSanctions.ts
@@ -3,9 +3,11 @@ import axios from 'axios'
 async function checkSanctions(address: string) {
     let res
     try {
-        res = await axios.get(`https://bot.dev.opendollar.com/api/screen?address=${address}`, {
+        res = await axios.get(`https://od-bots-git-feature-36-fix-cors-opendollar.vercel.app/api/screen?address=${address}`, {
             headers: {
+                'X-API-Key': process.env.REACT_APP_CHAINALYSIS_KEY as string,
                 Accept: 'application/json',
+
             },
         })
         return res.data

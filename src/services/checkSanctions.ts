a3ca78b23acf065e547a0d440695bf0ef610a3d1
jahabeebs <jahgoreds@gmail.com>
2023-09-28 13:19:48 -0500

    final url

--- src/services/checkSanctions.ts
+++ src/services/checkSanctions.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 async function checkSanctions(address: string) {
     let res
     try {
-        res = await axios.get(`https://od-bots-git-feature-36-fix-cors-opendollar.vercel.app/api/screen?address=${address}`, {
+        res = await axios.get(`https://bot.dev.opendollar.com/api/screen?address=${address}`, {
             headers: {
                 Accept: 'application/json',
             },

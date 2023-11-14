39e27217977bc5a62952da71b4d78fd94c2bb69d
jahabeebs <jahgoreds@gmail.com>
2023-11-14 13:37:01 -0500

    order

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -253,8 +253,8 @@ export const ratioChecker = (currentLiquidationRatio: number, liqRatio: number, 
     const currentLiquidationRatioAsDecimal = currentLiquidationRatio / 100
 
     if (currentLiquidationRatioAsDecimal === 0) return 0
-    if (currentLiquidationRatioAsDecimal < liqRatio) return 4
     if (currentLiquidationRatioAsDecimal === liqRatio) return 3
+    if (currentLiquidationRatioAsDecimal < liqRatio) return 4
     if (currentLiquidationRatioAsDecimal > liqRatio && currentLiquidationRatioAsDecimal <= safetyRatio) return 3
     if (currentLiquidationRatioAsDecimal > safetyRatio && currentLiquidationRatioAsDecimal <= safetyRatio * 1.2)
         return 2

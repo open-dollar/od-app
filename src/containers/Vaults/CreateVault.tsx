f24b6d67019c5123e7df36459c575ef124a8e752
kai-thompson <kai.thompsonaz@gmail.com>
2024-01-29 12:33:15 -0500

    Format balance with commas

--- src/containers/Vaults/CreateVault.tsx
+++ src/containers/Vaults/CreateVault.tsx
@@ -62,7 +62,11 @@ const CreateVault = ({
     }, [collaterals, tokensFetchedData])
 
     const collateralsDropdown = collaterals.map((collateral) => {
-        return { name: collateral.symbol, icon: getTokenLogo(collateral.symbol), value: formattedCollateralBalances[collateral.symbol] }
+        return {
+            name: collateral.symbol,
+            icon: getTokenLogo(collateral.symbol),
+            value: formatWithCommas(formattedCollateralBalances[collateral.symbol])
+        }
     })
 
     const dropdownSelected = collateralsDropdown.find((item) => item.name === selectedItem)!

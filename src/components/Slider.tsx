5e35ad452d6a62579f4a94192ec34bdb69bfb12c
kai-thompson <90171376+kai-thompson@users.noreply.github.com>
2024-02-03 01:35:56 -0500

    Issue #244: Deposit TVL Flow (#245)
    * Add first pass deposit TVL modal
    
    * Add styled slider based on design in issue (unused after redesign)
    
    * Minor cleanup
    
    * Improve button disabled/enabled logic to match designs
    
    * Don't use modal. I think I misunderstood the issue
    
    * Revert changes to modal component
    
    * Create page for collateral tvl deposit
    
    * Add empty TODO for onDeposit function
    
    * Post-QA slider fixes
    
    * Minor cleanup + Added 'Connect Wallet' state
    
    * Run yarn prettier on diff files
    
    * Small QA fixes in DepositFunds
    
    * More QA + Add unimplemented info icon
    
    * Use deposit amount USD value for right label
    
    * Change path for deposit

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,98 +1,33 @@
 import { useCallback } from 'react'
-import styled from 'styled-components/macro'
-
-const StyledRangeInput = styled.input<{ size: number }>`
-    -webkit-appearance: none; /* Hides the slider so that custom slider can be made */
-    width: 100%; /* Specific width is required for Firefox. */
-    background: transparent; /* Otherwise white in Chrome */
-    cursor: pointer;
-    &:focus {
-        outline: none;
-    }
-    &::-moz-focus-outer {
-        border: 0;
-    }
-    &::-webkit-slider-thumb {
-        -webkit-appearance: none;
-        height: ${({ size }) => size}px;
-        width: ${({ size }) => size}px;
-        background: ${({ theme }) => theme.colors.gradient};
-        border-radius: 100%;
-        border: none;
-        transform: translateY(-50%);
-        color: ${({ theme }) => theme.colors.gradient};
-        &:hover,
-        &:focus {
-            box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.1), 0px 4px 8px rgba(0, 0, 0, 0.08),
-                0px 16px 24px rgba(0, 0, 0, 0.06), 0px 24px 32px rgba(0, 0, 0, 0.04);
-        }
-    }
-    &::-moz-range-thumb {
-        height: ${({ size }) => size}px;
-        width: ${({ size }) => size}px;
-        background: ${({ theme }) => theme.colors.gradient};
-        border-radius: 100%;
-        border: none;
-        color: ${({ theme }) => theme.colors.gradient};
-        &:hover,
-        &:focus {
-            box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.1), 0px 4px 8px rgba(0, 0, 0, 0.08),
-                0px 16px 24px rgba(0, 0, 0, 0.06), 0px 24px 32px rgba(0, 0, 0, 0.04);
-        }
-    }
-    &::-ms-thumb {
-        height: ${({ size }) => size}px;
-        width: ${({ size }) => size}px;
-        background-color: ${({ theme }) => theme.colors.gradient};
-        border-radius: 100%;
-        color: ${({ theme }) => theme.colors.gradient};
-        &:hover,
-        &:focus {
-            box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.1), 0px 4px 8px rgba(0, 0, 0, 0.08),
-                0px 16px 24px rgba(0, 0, 0, 0.06), 0px 24px 32px rgba(0, 0, 0, 0.04);
-        }
-    }
-    &::-webkit-slider-runnable-track {
-        background: ${({ theme }) => theme.colors.gradient};
-        height: 2px;
-    }
-    &::-moz-range-track {
-        background: ${({ theme }) => theme.colors.gradient};
-        height: 2px;
-    }
-    &::-ms-track {
-        width: 100%;
-        border-color: transparent;
-        color: transparent;
-        background: ${({ theme }) => theme.colors.gradient};
-        height: 2px;
-    }
-    &::-ms-fill-lower {
-        background: ${({ theme }) => theme.colors.gradient};
-    }
-    &::-ms-fill-upper {
-        background: ${({ theme }) => theme.colors.gradient};
-    }
-`
+import styled from 'styled-components'
+import { useTheme } from 'styled-components'
+import { useTranslation } from 'react-i18next'
 
 interface InputSliderProps {
     value: number
     onChange: (value: number) => void
+    valueLabel?: string
+    valueUnits?: string
+    valueInfoLabel?: string
     step?: number
     min?: number
     max?: number
-    size?: number
 }
 
-export default function Slider({
+const Slider = ({
     value,
+    valueLabel,
+    valueUnits,
+    valueInfoLabel,
     onChange,
     min = 0,
     step = 1,
     max = 100,
-    size = 28,
     ...rest
-}: InputSliderProps) {
+}: InputSliderProps) => {
+    const { t } = useTranslation()
+    const { colors } = useTheme()
+
     const changeCallback = useCallback(
         (e) => {
             onChange(parseInt(e.target.value))
@@ -100,18 +35,174 @@ export default function Slider({
         [onChange]
     )
 
+    const slidePercentage = ((value - min) / (max - min)) * 100
+
     return (
-        <StyledRangeInput
-            size={size}
-            {...rest}
-            type="range"
-            value={value}
-            style={{ padding: '15px 0' }}
-            onChange={changeCallback}
-            aria-labelledby="input slider"
-            step={step}
-            min={min}
-            max={max}
-        />
+        <Container>
+            <InnerFlexContainer style={{ flexDirection: 'column' }}>
+                <SliderStepMarkersContainer>
+                    {Array.from(Array(max).keys()).map((_, index) => (
+                        <SliderTrackMarker
+                            key={index}
+                            style={{
+                                backgroundColor: index >= Math.max(value, min) ? colors.placeholder : 'transparent',
+                            }}
+                        />
+                    ))}
+                </SliderStepMarkersContainer>
+                <SliderTrack
+                    style={{
+                        background: `linear-gradient(to right, ${colors.blueish} ${slidePercentage}%, ${colors.colorPrimary} 0%)`,
+                    }}
+                >
+                    <Input
+                        {...rest}
+                        type="range"
+                        value={value}
+                        onChange={changeCallback}
+                        aria-labelledby="input slider"
+                        step={step}
+                        min={min}
+                        max={max}
+                    />
+                </SliderTrack>
+            </InnerFlexContainer>
+            <InnerFlexContainer>
+                {valueLabel && (
+                    <LabelContainer>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <LabelText>{t(valueLabel)}</LabelText>
+                            <ValueText>
+                                {value} {valueUnits && <span>{t(valueUnits)}</span>}
+                            </ValueText>
+                        </div>
+                        {valueInfoLabel && <LabelText>{t(valueInfoLabel)}</LabelText>}
+                    </LabelContainer>
+                )}
+            </InnerFlexContainer>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    width: 100%;
+    background-color: ${(props) => props.theme.colors.placeholder};
+    border: 1px solid ${(props) => props.theme.colors.border};
+    padding: 15px 15px;
+    border-radius: 10px;
+`
+
+const InnerFlexContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+`
+
+const SliderStepMarkersContainer = styled.div`
+    z-index: 1;
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    width: 95%;
+    transform: translateY(5.5px);
+`
+
+const LabelContainer = styled.div`
+    margin-top: 15px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    line-height: 1;
+`
+
+const LabelText = styled.span`
+    font-size: 13px;
+    color: ${({ theme }) => theme.colors.secondary};
+`
+
+const ValueText = styled.span`
+    font-size: 15px;
+    font-weight: 600;
+    margin-left: 5px;
+    color: ${({ theme }) => theme.colors.neutral};
+`
+
+const Input = styled.input`
+    -webkit-appearance: none;
+    background-color: transparent;
+    border: none;
+    width: 100%;
+    outline: none;
+    cursor: pointer;
+
+    &:focus {
+        outline: none;
+    }
+
+    &::-moz-focus-outer {
+        border: 0;
+    }
+
+    &::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        height: 18px;
+        width: 18px;
+        background: ${({ theme }) => theme.colors.blueish};
+        border-radius: 100%;
+        border: 3px;
+        border-style: solid;
+        border-color: ${({ theme }) => theme.colors.colorPrimary};
+        transform: translateY(-5.5px);
+    }
+
+    &::-moz-range-thumb {
+        height: 14px;
+        width: 14px;
+        background: ${({ theme }) => theme.colors.blueish};
+        border-radius: 100%;
+        border: 3px;
+        border-style: solid;
+        border-color: ${({ theme }) => theme.colors.colorPrimary};
+        transform: translateY(-35%);
+    }
+
+    &::-ms-thumb {
+        height: 18px;
+        width: 18px;
+        background-color: ${({ theme }) => theme.colors.blueish};
+        border-radius: 100%;
+        border: 3px;
+        border-style: solid;
+        border-color: ${({ theme }) => theme.colors.colorPrimary};
+        transform: translateY(-35%);
+    }
+
+    &::-webkit-slider-runnable-track {
+        -webkit-appearance: none;
+    }
+
+    &::-moz-range-track {
+        -webkit-appearance: none;
+    }
+
+    &::-ms-track {
+        -webkit-appearance: none;
+    }
+`
+
+const SliderTrack = styled.div`
+    position: relative;
+    height: 7px;
+    width: 100%;
+    border-radius: 10px;
+`
+
+const SliderTrackMarker = styled.div`
+    width: 3.5px;
+    height: 3.5px;
+    border-radius: 100%;
+`
+
+export default Slider
---
+++ src/containers/Deposit/DepositFunds.tsx
@@ -0,0 +1,259 @@
+import { useState, useMemo, useEffect } from 'react'
+import { ethers } from 'ethers'
+import { ArrowLeft, Info, AlertCircle } from 'react-feather'
+import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { useTheme } from 'styled-components'
+import { useTokenApproval, useProxyAddress, useActiveWeb3React } from '~/hooks'
+import { getTokenLogo, formatWithCommas, formatNumber } from '~/utils'
+import { useStoreState, useStoreActions } from '~/store'
+import { shortStringDate, isEmptyObject } from '~/utils'
+import { ApprovalState } from '~/hooks'
+import TokenInput from '~/components/TokenInput'
+import Button from '~/components/Button'
+
+const DepositFunds = ({ ...props }) => {
+    const { t } = useTranslation()
+    const { colors } = useTheme()
+    const history = useHistory()
+    const proxyAddress = useProxyAddress()
+
+    const { account, isActive } = useActiveWeb3React()
+
+    const [depositAmount, setDepositAmount] = useState('')
+
+    const tokenPath = props.match.params.token as string
+    const tokenSymbol = tokenPath.toUpperCase()
+
+    const {
+        safeModel: { liquidationData },
+        connectWalletModel: { tokensData, tokensFetchedData, isWrongNetwork },
+    } = useStoreState((state) => state)
+
+    const { popupsModel: popupsActions } = useStoreActions((state) => state)
+
+    const tokenData = tokensData?.[tokenSymbol]
+    const tokenFetchedData = tokensFetchedData?.[tokenSymbol]
+
+    const depositAssetUSDValue = formatNumber(
+        liquidationData?.collateralLiquidationData?.[tokenSymbol]?.currentPrice?.value || '0'
+    )
+
+    const depositAssetBalance = useMemo(
+        () => ethers.utils.formatEther(tokenFetchedData?.balanceE18 || '0'),
+        [tokenFetchedData?.balanceE18]
+    )
+
+    const depositAmountUSDValue = useMemo(
+        () =>
+            depositAmount && depositAssetUSDValue
+                ? formatNumber(String(Number(depositAmount) * Number(depositAssetUSDValue)))
+                : '',
+        [depositAmount, depositAssetUSDValue]
+    )
+
+    const [approvalState, approve] = useTokenApproval(
+        depositAmount,
+        tokenData?.address,
+        proxyAddress,
+        tokenFetchedData?.decimals,
+        true
+    )
+
+    const shouldDisplayApproveButton = approvalState !== ApprovalState.APPROVED || Number(depositAmount) === 0
+    const shouldDisplayConnectWalletButton = !account || !isActive
+
+    const shouldDisableApproveButton = Number(depositAmount) === 0 || isWrongNetwork
+    const shouldDisableDepositButton =
+        approvalState !== ApprovalState.APPROVED || Number(depositAmount) === 0 || isWrongNetwork
+
+    useEffect(() => {
+        if (!isEmptyObject(tokensData) && !tokensData?.[tokenSymbol]?.isCollateral) {
+            history.push('/404')
+        }
+    }, [history, tokenSymbol, tokensData])
+
+    // TODO: Implement onDeposit function once contracts are ready
+    const onDeposit = () => console.log('Deposit')
+
+    const onInfoClick = () => console.log('Info')
+
+    return (
+        <Container>
+            <Content>
+                <InnerContainer>
+                    <HeaderContainer>
+                        <ArrowLeft size="24" onClick={() => history.goBack()} cursor="pointer" />
+                        <HeaderText>Deposit funds</HeaderText>
+                    </HeaderContainer>
+                    <HorizontalSeparator />
+                    <InnerContainer style={{ marginBottom: 28 }}>
+                        <InputLabel>{t('amount')}</InputLabel>
+                        <TokenInput
+                            token={
+                                tokenData && {
+                                    icon: getTokenLogo(tokenData.symbol),
+                                    name: tokenData?.symbol,
+                                }
+                            }
+                            label={`Balance: ${formatWithCommas(depositAssetBalance)} ${tokenData?.symbol}`}
+                            rightLabel={`~$${formatWithCommas(depositAmountUSDValue)}`}
+                            onChange={(e) => setDepositAmount(e)}
+                            value={depositAmount}
+                            handleMaxClick={() => setDepositAmount(depositAssetBalance)}
+                        />
+                    </InnerContainer>
+                    <InnerContainer>
+                        {/* TODO: Use real date values */}
+                        <DateInfoContainer>
+                            <DateInfoLabel>{t('pool_start_date')}</DateInfoLabel>
+                            <DateInfoValue>{shortStringDate(1675234410000)}</DateInfoValue>
+                        </DateInfoContainer>
+                        <DateInfoContainer>
+                            <DateInfoLabel>{t('current_date')}</DateInfoLabel>
+                            <DateInfoValue>{shortStringDate(1675061610000)}</DateInfoValue>
+                        </DateInfoContainer>
+                        <DateInfoContainer>
+                            <InnerContainer style={{ display: 'flex', alignItems: 'center' }}>
+                                <DateInfoLabel>{t('tokens_will_be_unlocked')}</DateInfoLabel>
+                                <Info
+                                    size="16"
+                                    color={colors.blueish}
+                                    style={{ marginLeft: 6 }}
+                                    cursor="pointer"
+                                    onClick={onInfoClick}
+                                />
+                            </InnerContainer>
+                            <DateInfoValue>{shortStringDate(1677653610000)}</DateInfoValue>
+                        </DateInfoContainer>
+                    </InnerContainer>
+                    <WarningLabelContainer>
+                        <InnerContainer style={{ display: 'flex', alignItems: 'flex-start' }}>
+                            <AlertCircle size="24" color={colors.yellowish} style={{ height: 'auto', marginTop: 1 }} />
+                            <WarningLabel>{t('deposit_funds_warning')}</WarningLabel>
+                        </InnerContainer>
+                    </WarningLabelContainer>
+                    <InnerContainer>
+                        {shouldDisplayConnectWalletButton ? (
+                            <Button
+                                text="connect_wallet"
+                                onClick={() => popupsActions.setIsConnectorsWalletOpen(true)}
+                                style={{ width: '100%' }}
+                                disabled={isWrongNetwork}
+                            />
+                        ) : (
+                            <ButtonContainer>
+                                {shouldDisplayApproveButton && (
+                                    <Button
+                                        text={t('approve_token', { symbol: tokenData?.symbol })}
+                                        onClick={approve}
+                                        style={{ width: '100%' }}
+                                        disabled={shouldDisableApproveButton}
+                                    />
+                                )}
+                                <Button
+                                    text="deposit"
+                                    onClick={onDeposit}
+                                    style={{ width: '100%' }}
+                                    disabled={shouldDisableDepositButton}
+                                />
+                            </ButtonContainer>
+                        )}
+                    </InnerContainer>
+                </InnerContainer>
+            </Content>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    max-width: 498px;
+    margin: 120px auto;
+    padding: 0 15px;
+    @media (max-width: 767px) {
+        margin: 50px auto;
+    }
+`
+
+const Content = styled.div`
+    background-color: ${(props) => props.theme.colors.colorPrimary};
+    border-radius: 20px;
+    padding: 30px 32px;
+`
+
+const InnerContainer = styled.div``
+
+const HeaderContainer = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const DateInfoContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    margin-bottom: 5px;
+`
+
+const WarningLabelContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    background-color: ${(props) => `${props.theme.colors.yellowish}40`};
+    border-radius: 6px;
+    padding: 12px 14px 12px 14px;
+    margin: 30px 0;
+`
+
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    gap: 10px;
+`
+
+const WarningLabel = styled.p`
+    font-size: ${(props) => props.theme.font.small};
+    font-weight: 500;
+    color: ${(props) => props.theme.colors.yellowish};
+    margin-left: 6px;
+    line-height: 18.2px;
+`
+
+const HeaderText = styled.span`
+    flex-grow: 1;
+    text-align: center;
+    font-weight: 600;
+    padding: 0 24px 0 0;
+    font-size: ${(props) => props.theme.font.medium};
+`
+
+const HorizontalSeparator = styled.span`
+    display: block;
+    width: 100%;
+    height: 0.5px;
+    background-color: #00587e;
+    margin: 26px 0;
+`
+
+const InputLabel = styled.h2`
+    font-size: ${(props) => props.theme.font.default};
+    font-weight: 600;
+    color: ${(props) => props.theme.colors.neutral};
+    margin-bottom: 10px;
+`
+
+const DateInfoLabel = styled.span`
+    font-size: ${(props) => props.theme.font.small};
+    color: ${(props) => props.theme.colors.secondary};
+`
+
+const DateInfoValue = styled.span`
+    font-size: ${(props) => props.theme.font.small};
+    font-weight: 600;
+    color: ${(props) => props.theme.colors.neutral};
+`
+
+export default DepositFunds

1632d1e3a076a12e61b3df4adc3c4b83ecf6564b
jahabeebs <jahgoreds@gmail.com>
2024-04-01 18:55:35 -0400

    add unit tests and fix existing tests

--- src/components/DecimalInput.tsx
+++ src/components/DecimalInput.tsx
@@ -16,6 +16,21 @@ interface Props {
     data_test_id?: string
 }
 
+export function processInputValue(inputValue: string): string {
+    const decimalRegex = /^-?\d+(\.\d{1,4})?$/;
+    let processedValue = inputValue;
+    if (decimalRegex.test(inputValue)) {
+        if (inputValue.startsWith('0') && inputValue.charAt(1) !== '.') {
+            processedValue = inputValue.replace(/(\d)(?=(\d))/, '$1.');
+        } else if (inputValue.startsWith('.')) {
+            processedValue = '0' + inputValue;
+        } else if (inputValue.length === 6 && Number(inputValue) === 0) {
+            processedValue = '';
+        }
+    }
+    return processedValue;
+}
+
 const DecimalInput = ({
     label,
     placeholder,
@@ -34,23 +49,11 @@ const DecimalInput = ({
     const [length, setLength] = useState(16)
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const val = e.target.value
-        const decimalRegex = /^-?\d+(\.\d{1,4})?$/
-
-        if (decimalRegex.test(val)) {
-            val.includes('.') ? setLength(17) : setLength(16)
-            if (val.startsWith('0') && val.charAt(1) !== '.') {
-                const returnedVal = val.replace(/(\d)(?=(\d))/, '$1.')
-                onChange(returnedVal)
-            } else if (val.startsWith('.')) {
-                onChange('0' + val)
-            } else if (val.length === 6 && Number(val) === 0) {
-                onChange('')
-            } else {
-                onChange(val)
-            }
-        }
-    }
+        const val = e.target.value;
+        const processedValue = processInputValue(val);
+        setLength(val.includes('.') ? 17 : 16);
+        onChange(processedValue);
+    };
 
     return (
         <Container>

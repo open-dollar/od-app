30f83c799aa40a280c450cacc45edeef3142c60a
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-04-03 12:11:45 -0400

    chain IDs, and url params

--- src/utils/formatBridgeUrl.ts
+++ src/utils/formatBridgeUrl.ts
@@ -1,17 +1,16 @@
-import { from } from "@apollo/client"
+export const chainIdToName = new Map<number, string>([
+  [1, 'ethereum'],
+  [42161, 'arbitrum-one'],
+  [56, 'bsc'],
+  [137, 'matic'],
+  [250, 'fantom'],
+]);
 
-const chainIdToName = {
-  1: 'ethereum',
-  42161: 'arbitrum-one',
-  56: 'bsc',
-  137: 'matic',
-  250: 'fantom',
-}
-
-export const formatBridgeUrl = (tokenAddress: string, amount: string, fromChain?: string, fromChainId?: number) => {
+export const formatBridgeUrl = (amount: string, tokenAddress: string, fromChain?: string, fromChainId?: number) => {
   if (fromChain == 'ethereum' || fromChainId == 1) {
-    // @ts-ignore
-    const chain: string = fromChain ? fromChain : fromChainId ? chainIdToName[fromChainId as keyof chainIdToName] : ''
     return `https://bridge.arbitrum.io/?token-${tokenAddress}&amount=${amount}&destinationChain=arbitrum-one&sourceChain=ethereum`
+  }
+  if (fromChainId !== 1) {
+    return `https://jumper.exchange/?fromAmount=${amount}&fromChain=${fromChainId}&fromToken=${tokenAddress}&toChain=42161&toToken=0x5979d7b546e38e414f7e9822514be443a4800529`
   }
 }

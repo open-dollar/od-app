53ac8fe5a3c7d03a661ce7220dbf3c53f60ac403
alekseevaiana <alekseevadev@gmail.com>
2024-04-05 08:49:50 -0700

    layout of step content

--- src/components/StepsContent.tsx
+++ src/components/StepsContent.tsx
@@ -21,21 +21,38 @@ const StepsContent = ({ title, text, stepNumber, btnText, handleClick, isDisable
         { title: 'Step 2', text: 'Create Vault Facilitator' },
         { title: 'Step 3', text: 'Create a Vault' },
     ]
+
+    const returnLottie = (step: number) => {
+        switch (step) {
+            case 1:
+                return <img src={require('../assets/wallet.png')} alt="" />
+            case 2:
+                return <img src={require('../assets/od-vault.png')} alt="" />
+            default:
+                return <img src={require('../assets/od-land.png')} alt="" />
+        }
+    }
+
     return (
         <Container id={id}>
             <StepperWrapper>
-                <Stepper step={stepNumber} steps={steps}/>
+                <Stepper step={stepNumber} steps={steps} />
             </StepperWrapper>
-            <Title>{t(title)}</Title>
-            <Text>{t(text)}</Text>
-            <Button
-                data-test-id="steps-btn"
-                id={stepNumber === 2 ? 'create-safe' : ''}
-                disabled={isDisabled || isLoading}
-                isLoading={isLoading}
-                text={t(btnText)}
-                onClick={handleClick}
-            />
+            <ContentContainer>
+                <ImageContainer>{returnLottie(stepNumber)}</ImageContainer>
+                <ContentWrapper>
+                    <Title>{t(title)}</Title>
+                    <Text>{t(text)}</Text>
+                    <Button
+                        data-test-id="steps-btn"
+                        id={stepNumber === 2 ? 'create-safe' : ''}
+                        disabled={isDisabled || isLoading}
+                        isLoading={isLoading}
+                        text={t(btnText)}
+                        onClick={handleClick}
+                    />
+                </ContentWrapper>
+            </ContentContainer>
         </Container>
     )
 }
@@ -46,7 +63,21 @@ const Container = styled.div`
     text-align: center;
     margin-top: 20px;
     display: flex;
+    flex-direction: column;
     align-items: center;
+`
+
+const ImageContainer = styled.div``
+
+const ContentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`
+
+const ContentContainer = styled.div`
+    display: flex;
+    max-width: 923px;
 `
 
 const StepperWrapper = styled.div`
@@ -56,20 +87,27 @@ const StepperWrapper = styled.div`
     padding-right: 28px;
     background-color: white;
     border-radius: 4px;
-    box-shadow: 0px 4px 6px 0px #0D4B9D33;
+    box-shadow: 0px 4px 6px 0px #0d4b9d33;
     width: fit-content;
+    margin-bottom: 105px;
 `
 
 const Title = styled.div`
-    font-size: ${(props) => props.theme.font.large};
+    /* font-size: ${(props) => props.theme.font.large}; */
+    font-size: 28px;
     font-weight: 600;
-    color: ${(props) => props.theme.colors.neutral};
+    /* color: ${(props) => props.theme.colors.neutral}; */
+    color: #1c293a;
     margin-bottom: 10px;
 `
 
 const Text = styled.div`
-    font-size: ${(props) => props.theme.font.small};
-    color: ${(props) => props.theme.colors.secondary};
+    /* font-size: ${(props) => props.theme.font.small}; */
+    font-size: 18px;
+    text-align: start;
+    /* color: ${(props) => props.theme.colors.secondary}; */
+    color: #475662;
     margin-bottom: 20px;
     line-height: 21px;
+    max-width: 550px;
 `

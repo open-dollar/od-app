1da7a3d78a5850a6aa10c3a40ff78bd438122c86
Iana <36742189+alekseevaiana@users.noreply.github.com>
2024-04-16 09:22:19 -0700

    Issue #293. App Redesign: stats component on modify vaults page (#356)
    * switch to light theme
    
    * headers colors
    
    * rm img
    
    * card's box shadows and borders
    
    * data table bgs
    
    * fonts
    
    * contracts table styles
    
    * prettier
    
    * lint fix
    
    * fix fonts on cards
    
    * add images
    
    * bg gradient

--- src/containers/Analytics/ContractsTable.tsx
+++ src/containers/Analytics/ContractsTable.tsx
@@ -4,8 +4,6 @@ import { Tooltip as ReactTooltip } from 'react-tooltip'
 
 import { Container, Content, Head, Heads, HeadsContainer, List, ListItemLabel, SectionContent } from './DataTable'
 import { AddressLink } from '~/components/AddressLink'
-import CopyIconBlue from '~/components/Icons/CopyIconBlue'
-import { useState } from 'react'
 
 interface ContractsTableProps {
     title: string
@@ -54,27 +52,10 @@ const reorderRows = (rows: string[][]) => {
 }
 
 export const ContractsTable = ({ title, colums, rows }: ContractsTableProps) => {
-    const [tooltips, setTooltips] = useState<{ [key: string]: string }>({})
     const { chainId } = useWeb3React()
 
     const reorderedColumns = reorderColumns(colums)
     const reorderedRows = reorderRows(rows)
-
-    const handleCopyAddress = (address: string) => {
-        navigator.clipboard.writeText(address || '')
-
-        setTooltips((prevTooltips) => ({
-            ...prevTooltips,
-            [address]: 'Copied',
-        }))
-
-        setTimeout(() => {
-            setTooltips((prevTooltips) => ({
-                ...prevTooltips,
-                [address]: 'Copy',
-            }))
-        }, 2000)
-    }
 
     return (
         <Container>
@@ -101,13 +82,6 @@ export const ContractsTable = ({ title, colums, rows }: ContractsTableProps) => 
                                         {valueIndex === 2 && (
                                             <AddressColumm>
                                                 <AddressLink address={value} chainId={chainId || 420} />
-                                                <WrapperIcon
-                                                    data-tooltip-content={tooltips[value] || 'Copy'}
-                                                    data-tooltip-id={value}
-                                                    onClick={() => handleCopyAddress(value)}
-                                                >
-                                                    <CopyIconBlue />
-                                                </WrapperIcon>
                                             </AddressColumm>
                                         )}
                                     </SListItem>
@@ -159,6 +133,7 @@ const SList = styled(List)`
     @media (min-width: 783px) {
         div:nth-child(2) div {
             width: 100%;
+            font-weight: 500;
         }
 
         div:nth-child(2) {
@@ -194,23 +169,20 @@ const SListItem = styled.div<ListItemProps>`
     text-align: start;
     text-overflow: ellipsis;
     font-size: 16px;
-    font-weight: 400;
+    font-weight: 700;
     border-radius: 4px;
     & a {
-        color: white;
+        color: ${(props) => props.theme.colors.tertiary};
         font-size: 16px;
-        font-weight: 400;
+        font-weight: 700;
     }
     width: 174px;
-    color: ${(props) => props.theme.colors.customSecondary};
+    color: ${(props) => props.theme.colors.tertiary};
     padding: 15px 10px;
     &:first-child {
         padding: 15px 25px;
     }
 
-    &:nth-child(1) {
-        background-color: #002b40;
-    }
     ${({ theme }) => theme.mediaWidth.upToSmall`
       &:first-child {
         padding: 15px 20px;
@@ -220,12 +192,4 @@ const SListItem = styled.div<ListItemProps>`
     flex: 0 0 100%;
     min-width:50%;
   `}
-`
-
-const WrapperIcon = styled.div`
-    display: flex;
-    justify-content: center;
-    margin-left: 16px;
-    width: 20px;
-    height: 20px;
 `

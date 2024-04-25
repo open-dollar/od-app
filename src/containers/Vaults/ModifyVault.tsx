7a1a49d8666d86127129f5a9f4b1ebf27402bf87
jahabeebs <jahgoreds@gmail.com>
2024-04-25 12:30:18 -0500

    approvals working

--- src/containers/Vaults/ModifyVault.tsx
+++ src/containers/Vaults/ModifyVault.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { BigNumber, ethers } from 'ethers'
 import styled from 'styled-components'
 import { DEFAULT_SAFE_STATE, formatNumber, formatWithCommas, getTokenLogo } from '~/utils'
-import useGeb from '~/hooks/useGeb'
+import useGeb, { useProxyAddress } from '~/hooks/useGeb'
 import Review from './Review'
 import { useStoreActions, useStoreState } from '~/store'
 import TokenInput from '~/components/TokenInput'
@@ -15,6 +15,8 @@ import {
     useActiveWeb3React,
     useInputsHandlers,
     useSafeInfo,
+    useTokenApproval,
+    ApprovalState,
 } from '~/hooks'
 
 const ModifyVault = ({ isDeposit, isOwner, vaultId }: { isDeposit: boolean; isOwner: boolean; vaultId: string }) => {
@@ -84,6 +86,25 @@ const ModifyVault = ({ isDeposit, isOwner, vaultId }: { isDeposit: boolean; isOw
     const haiBalance = ethers.utils.formatEther(tokenBalances.OD?.balanceE18 || '0')
 
     const haiBalanceUSD = useTokenBalanceInUSD('OD', rightInput ? rightInput : availableHai)
+
+    const proxyAddress = useProxyAddress()
+
+    const [collateralUnlockState, collateralApproveUnlock] = useTokenApproval(
+        parsedAmounts.leftInput,
+        singleSafe ? tokensData[singleSafe?.collateralName!].address : undefined,
+        proxyAddress,
+        selectedTokenDecimals,
+        true
+    )
+
+    const [unlockState, approveUnlock] = useTokenApproval(
+        parsedAmounts.rightInput,
+        tokensData?.OD.address,
+        proxyAddress,
+        '18',
+        true,
+        parsedAmounts.rightInput === availableHai && availableHai !== '0'
+    )
 
     const onMaxLeftInput = () => {
         if (isDeposit) {
@@ -304,10 +325,34 @@ const ModifyVault = ({ isDeposit, isOwner, vaultId }: { isDeposit: boolean; isOw
                         </Inner>
                         <Row>
                             <ButtonContainer>
-                                <Button onClick={handleSubmit} disabled={!isValid} maxSize={'250px'}>
-                                    {'Review Transaction'}
-                                </Button>
+                                {isDeposit ? (
+                                    collateralUnlockState === ApprovalState.PENDING ||
+                                    collateralUnlockState === ApprovalState.NOT_APPROVED ? (
+                                        <Button
+                                            onClick={collateralApproveUnlock}
+                                            disabled={collateralUnlockState === ApprovalState.PENDING}
+                                        >
+                                            {collateralUnlockState === ApprovalState.PENDING
+                                                ? 'Unlocking...'
+                                                : `Unlock ${singleSafe?.collateralName}`}
+                                        </Button>
+                                    ) : (
+                                        <Button onClick={handleSubmit} disabled={!isValid}>
+                                            Review Transaction
+                                        </Button>
+                                    )
+                                ) : unlockState === ApprovalState.PENDING ||
+                                  unlockState === ApprovalState.NOT_APPROVED ? (
+                                    <Button onClick={approveUnlock} disabled={unlockState === ApprovalState.PENDING}>
+                                        {unlockState === ApprovalState.PENDING ? 'Approving...' : 'Approve OD'}
+                                    </Button>
+                                ) : (
+                                    <Button onClick={handleSubmit} disabled={!isValid}>
+                                        Review Transaction
+                                    </Button>
+                                )}
                             </ButtonContainer>
+
                             {error && (
                                 <ErrorContainer>
                                     <p>Error: {error}</p>

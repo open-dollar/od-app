583518556cf7d0682b7cf03acdb2ee4a0de38102
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-04-25 13:26:28 -0400

    QA: Post Launch - fix reported bugs (#410)
    * fix issues with text and memoise
    
    * fix bunch of small issues
    
    * remove testnet

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { useWeb3React } from '@web3-react/core'
 import { useTranslation } from 'react-i18next'
 import styled, { useTheme } from 'styled-components'
 
-import { formatDataNumber, getTokenLogo, newTransactionsFirst, returnWalletAddress } from '~/utils'
+import { formatDataNumber, newTransactionsFirst, returnWalletAddress } from '~/utils'
 import { useStoreActions, useStoreState } from '~/store'
 import { isTransactionRecent } from '~/hooks'
 import Identicon from './Icons/Identicon'
@@ -87,11 +87,6 @@ const Navbar = () => {
         } catch (error) {
             console.log('Error adding ODG to the wallet:', error)
         }
-    }
-
-    const handleDollarClick = () => {
-        // Disable temporarily
-        // setPopupVisibility(!isPopupVisible)
     }
 
     const handleClickOutsideOdRef = (event: MouseEvent) => {

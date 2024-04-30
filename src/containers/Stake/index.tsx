b84544ff2b71dfd7d38d8b8fe63d62103ac881be
alekseevaiana <alekseevadev@gmail.com>
2024-04-30 13:57:29 -0700

    basic stake component

--- src/containers/Stake/index.tsx
+++ src/containers/Stake/index.tsx
@@ -1,5 +1,37 @@
+import styled from 'styled-components'
+
 const Stake = () => {
-    return <div>Stake</div>
+    return (
+        <Container>
+            <Title>Stake</Title>
+            <Description>Stake assets in the Camelot to earn ODG rewards.</Description>
+            <Pools>Active Pools</Pools>
+        </Container>
+    )
 }
+
+const Container = styled.div`
+    max-width: 1362px;
+    margin: 80px auto;
+    padding: 0 15px;
+    @media (max-width: 767px) {
+        margin: 50px auto;
+    }
+`
+
+const Title = styled.h2`
+    font-size: 34px;
+    font-weight: 700;
+    font-family: ${(props) => props.theme.family.headers};
+
+    color: ${(props) => props.theme.colors.accent};
+`
+
+const Description = styled.div`
+    font-size: ${(props) => props.theme.font.default};
+    color: ${(props) => props.theme.colors.accent};
+`
+
+const Pools = styled.div``
 
 export default Stake

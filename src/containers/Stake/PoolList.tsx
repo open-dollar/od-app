08017e6586f1a6e03583a7718d247954551115d3
alekseevaiana <alekseevadev@gmail.com>
2024-05-02 11:23:47 -0700

    basic pool block

---
+++ src/containers/Stake/PoolList.tsx
@@ -0,0 +1,155 @@
+import { useMemo, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import styled from 'styled-components'
+
+import { useStoreState } from '~/store'
+import LinkButton from '~/components/LinkButton'
+import VaultBlock from '~/components/VaultBlock'
+import CheckBox from '~/components/CheckBox'
+import { returnState, ISafe } from '~/utils'
+import { useActiveWeb3React } from '~/hooks'
+
+const PoolList = ({ address }: { address?: string }) => {
+    const [showEmpty, setShowEmpty] = useState(true)
+
+    const { account } = useActiveWeb3React()
+
+    const { t } = useTranslation()
+
+    const { connectWalletModel: connectWalletState, safeModel: safeState } = useStoreState((state) => state)
+
+    const safes = useMemo(() => {
+        if (safeState.list.length > 0) {
+            return showEmpty ? safeState.list : safeState.list.filter((safe) => returnState(safe.riskState) !== '')
+        }
+        return []
+    }, [safeState.list, showEmpty])
+
+    const isOwner = useMemo(
+        () => (address && account ? account.toLowerCase() === address.toLowerCase() : true),
+        [account, address]
+    )
+
+    const returnSafeList = () => {
+        // if (safeState.list.length > 0) {
+            return (
+                <Container>
+                    <Header>
+                        <Col className="first-col">
+                            <Title>Pools</Title>
+                        </Col>
+                        <Col className={'last-col'}>
+                            Show in Camelot
+                        </Col>
+                    </Header>
+                    <SafeBlocks>
+                        {safes.map((safe: ISafe) => (
+                            <div key={safe.id}>
+                                {safe.collateralName && <VaultBlock className="safeBlock" {...safe} />}
+                            </div>
+                        ))}
+                    </SafeBlocks>
+                </Container>
+            )
+        // }
+        // return null
+    }
+
+    return <>{returnSafeList()}</>
+}
+
+export default PoolList
+
+const Container = styled.div`
+    max-width: 1360px;
+    margin: 80px auto;
+    padding: 0 15px;
+    @media (max-width: 767px) {
+        margin: 50px auto;
+    }
+`
+
+const LinkBtnContainer = styled.div`
+    a {
+        color: white;
+        outline: none;
+        cursor: pointer;
+        min-width: 100px;
+        padding: 12px 40px;
+        font-weight: 700;
+        background: ${(props) => props.theme.colors.gradientBg};
+        border-radius: 3px;
+        transition: all 0.3s ease;
+        font-family: ${(props) => props.theme.family.headers};
+    }
+
+    &:hover {
+        opacity: 0.8;
+    }
+`
+
+const SafeBlocks = styled.div`
+    border-radius: 8px;
+`
+
+const Title = styled.div`
+    font-weight: 700;
+    font-size: 34px;
+    font-family: ${(props) => props.theme.family.headers};
+    color: ${(props) => props.theme.colors.accent};
+`
+
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 30px;
+    &.safesList {
+        padding: 0 20px;
+        margin: 20px 0;
+    }
+
+    @media (max-width: 767px) {
+        flex-direction: column;
+    }
+`
+const Col = styled.div`
+    &.first-col {
+        margin-right: 33px;
+    }
+
+    &.last-col {
+        margin-left: auto;
+    }
+
+    @media (max-width: 767px) {
+        &.last-col {
+            margin-left: 0;
+            margin-top: 30px;
+        }
+    }
+`
+
+const BtnInner = styled.div`
+    display: flex;
+    align-items: center;
+    svg {
+        margin-right: 5px;
+    }
+`
+
+const CheckboxContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    margin-top: 5px;
+    span {
+        margin-left: 10px;
+        position: relative;
+        font-size: 12px;
+        text-transform: uppercase;
+        font-weight: 700;
+        color: ${(props) => props.theme.colors.accent};
+        letter-spacing: 3px;
+    }
+`
--- src/containers/Stake/index.tsx
+++ src/containers/Stake/index.tsx
@@ -1,11 +1,42 @@
 import styled from 'styled-components'
+import PoolList from './PoolList'
+import { getTokenLogo } from '~/utils'
 
 const Stake = () => {
     return (
         <Container>
-            <Title>Stake</Title>
-            <Description>Stake assets in the Camelot to earn ODG rewards.</Description>
-            <Pools>Active Pools</Pools>
+            <Title>Earn</Title>
+            <Pools>
+                <BlockContainer>
+                    <BlockHeader>
+                        <PoolInfo>
+                            <PoolData>
+                                <PoolTitle>OD-ETH</PoolTitle>
+                                <img src={getTokenLogo('OD')} alt={''} width={'50px'} />
+                                <img src={getTokenLogo('ETH')} alt={''} width={'50px'} />
+                            </PoolData>
+                        </PoolInfo>
+                    </BlockHeader>
+                    <Block>
+                        <Item>
+                            <Label>Status</Label>
+                            <Value>Active</Value>
+                        </Item>
+                        <Item>
+                            <Label>TVL</Label>
+                            <Value>$120,000</Value>
+                        </Item>
+                        <Item>
+                            <Label>APR</Label>
+                            <Value>134.99%</Value>
+                        </Item>
+                        <Item>
+                            <Label>Revard</Label>
+                            <Value>ST-OD-ARB</Value>
+                        </Item>
+                    </Block>
+                </BlockContainer>
+            </Pools>
         </Container>
     )
 }
@@ -24,14 +55,134 @@ const Title = styled.h2`
     font-weight: 700;
     font-family: ${(props) => props.theme.family.headers};
 
-    color: ${(props) => props.theme.colors.accent};
-`
+    margin-bottom: 30px;
 
-const Description = styled.div`
-    font-size: ${(props) => props.theme.font.default};
     color: ${(props) => props.theme.colors.accent};
 `
 
 const Pools = styled.div``
+
+const BlockContainer = styled.div`
+    border-radius: 4px;
+    margin-bottom: 29px;
+    background: white;
+    box-shadow: 0px 4px 6px 0px #0d4b9d33;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    &.empty {
+        background: white;
+    }
+`
+
+const BlockHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #1c293a33;
+    padding-left: 34px;
+    padding-top: 22px;
+    padding-bottom: 11px;
+    padding-right: 34px;
+`
+
+const PoolInfo = styled.div`
+    display: flex;
+    align-items: center;
+    svg {
+        border-radius: ${(props) => props.theme.global.borderRadius};
+        border: 1px solid ${(props) => props.theme.colors.border};
+        ${({ theme }) => theme.mediaWidth.upToSmall`
+    width: 25px;
+    height: 25px;
+  `}
+    }
+`
+
+const PoolData = styled.div`
+    display: flex;
+    ${({ theme }) => theme.mediaWidth.upToSmall`
+    margin-left: 10px;
+  `}
+`
+
+const PoolTitle = styled.div`
+    font-size: ${(props) => props.theme.font.large};
+    font-family: ${(props) => props.theme.family.headers};
+    color: ${(props) => props.theme.colors.accent};
+    font-weight: 700;
+
+    margin-right: 10px;
+
+    span {
+        font-weight: 500;
+        color: ${(props) => props.theme.colors.primary};
+    }
+`
+
+const Block = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    gap: 150px;
+
+    padding-left: 34px;
+    padding-top: 19px;
+    padding-bottom: 22px;
+    padding-right: 34px;
+    @media (max-width: 767px) {
+        display: block;
+        margin-top: 10px;
+        &:last-child {
+            border-bottom: 0;
+        }
+    }
+`
+
+const Item = styled.div`
+    min-width: 200px;
+    @media (max-width: 767px) {
+        display: flex;
+        width: auto;
+        align-items: center;
+        justify-content: space-between;
+        margin: 0 0 3px 0;
+        &:last-child {
+            margin-bottom: 0;
+        }
+    }
+
+    &.low div:last-child {
+        color: #459d00;
+    }
+
+    &.elevated div:last-child {
+        color: #ffaf1d;
+    }
+
+    &.high div:last-child {
+        color: #e75966;
+    }
+
+    &.liquidation div:last-child {
+        color: #e75966;
+    }
+`
+
+const Label = styled.div`
+    font-size: ${(props) => props.theme.font.default};
+    color: ${(props) => props.theme.colors.tertiary};
+    font-weight: 400;
+    @media (max-width: 767px) {
+        font-size: ${(props) => props.theme.font.small};
+    }
+`
+
+const Value = styled.div`
+    font-size: ${(props) => props.theme.font.default};
+    color: ${(props) => props.theme.colors.accent};
+    font-weight: 700;
+    @media (max-width: 767px) {
+        font-size: ${(props) => props.theme.font.small};
+    }
+`
 
 export default Stake

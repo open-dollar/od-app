b6201dda27878a82ca1675a0d69363c4146e340f
Patrick Gallagher <blockchainbuddha@gmail.com>
2024-05-05 12:24:23 -0700

    Fix button color prop

--- src/chains.ts
+++ src/chains.ts
@@ -54,10 +54,14 @@ export function getAddChainParameters(chainId: number): AddEthereumChainParamete
 }
 
 type ChainConfig = { [chainId: number]: BasicChainInformation | ExtendedChainInformation }
-
+console.log('foo')
 export const MAINNET_CHAINS: ChainConfig = {
     42161: {
-        urls: ['https://arbitrum.blockpi.network/v1/rpc/public'],
+        urls: [
+            process.env.REACT_APP_NETWORK_URL
+                ? process.env.REACT_APP_NETWORK_URL
+                : 'https://arbitrum.blockpi.network/v1/rpc/public',
+        ],
         name: 'Arbitrum One',
         nativeCurrency: ETH,
         blockExplorerUrls: ['https://arbiscan.io'],
@@ -72,7 +76,11 @@ export const MAINNET_CHAINS: ChainConfig = {
 
 export const TESTNET_CHAINS: ChainConfig = {
     421614: {
-        urls: ['https://arbitrum-sepolia.blockpi.network/v1/rpc/public'],
+        urls: [
+            process.env.REACT_APP_NETWORK_URL
+                ? process.env.REACT_APP_NETWORK_URL
+                : 'https://arbitrum-sepolia.blockpi.network/v1/rpc/public',
+        ],
         name: 'Arbitrum Sepolia',
         nativeCurrency: ETH,
         blockExplorerUrls: ['https://sepolia.arbiscan.io/'],

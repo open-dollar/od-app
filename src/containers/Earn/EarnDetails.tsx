4e7a399b264652246b80ed6496fa9e7e44acb4c7
alekseevaiana <alekseevadev@gmail.com>
2024-05-14 12:07:04 -0700

    calculate time periods

--- src/containers/Earn/EarnDetails.tsx
+++ src/containers/Earn/EarnDetails.tsx
@@ -68,15 +68,54 @@ const EarnDetails = () => {
         history.push(`/earn`)
     }, [history])
 
-    const getTimePeriod = () => {
+    const getDates = () => {
         const start = new Date(Number(nitroPool?.settings.startTime) * 1000)
         const end = new Date(Number(nitroPool?.settings.endTime) * 1000)
         const now = new Date()
+        return { start, end, now }
+    }
+
+    const getTimePeriod = () => {
+        const { start, end, now } = getDates()
         return now > start && now < end ? 'Active' : 'Inactive'
     }
 
     const getDuration = () => {
-        
+        const { start, end } = getDates()
+
+        const yearsDifference = end.getFullYear() - start.getFullYear()
+        const monthsDifference = end.getMonth() - start.getMonth()
+        let totalMonths = yearsDifference * 12 + monthsDifference
+
+        const startCopy = new Date(start)
+        startCopy.setMonth(startCopy.getMonth() + totalMonths)
+
+        let remainingDays = (end.getTime() - startCopy.getTime()) / (1000 * 60 * 60 * 24)
+
+        if (remainingDays < 0) {
+            totalMonths -= 1
+            startCopy.setMonth(startCopy.getMonth() - 1)
+            remainingDays = (end.getTime() - startCopy.getTime()) / (1000 * 60 * 60 * 24)
+        }
+
+        return `${totalMonths} months ${Math.floor(remainingDays)} days`
+    }
+
+    const getEndDuration = (): string => {
+        const { end, now } = getDates()
+
+        const diffMs = end.getTime() - now.getTime()
+
+        let diffSec = Math.floor(diffMs / 1000)
+        const diffMin = Math.floor(diffSec / 60)
+        const diffHrs = Math.floor(diffMin / 60)
+        const diffDays = Math.floor(diffHrs / 24)
+
+        diffSec = diffSec % 60
+        const remainingMin = diffMin % 60
+        const remainingHrs = diffHrs % 24
+
+        return `${diffDays} D ${remainingHrs}h ${remainingMin}min ${diffSec} sec`
     }
 
     return (
@@ -124,11 +163,11 @@ const EarnDetails = () => {
                                 </Item>
                                 <Item>
                                     <Label>Duration</Label>
-                                    <Value>12 months 6 days</Value>
+                                    <Value>{getDuration()}</Value>
                                 </Item>
                                 <Item>
                                     <Label>End in</Label>
-                                    <Value>57 D 1h 27min 13 sec</Value>
+                                    <Value>{getEndDuration()}</Value>
                                 </Item>
                             </ColWrapper>
                         </Wrapper>

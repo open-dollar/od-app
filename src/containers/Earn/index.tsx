60f2254f1ee0e76d32a5da467bcbc24c5bea42ae
Iana <36742189+alekseevaiana@users.noreply.github.com>
2024-05-17 12:38:23 -0700

    Issue #486. Earn page improvements (#490)
    * add content
    
    * styles
    
    * change text color to accent
    
    * new line for p
    
    * add loader
    
    * prettier
    
    * add btn and add second header
    
    * no need for loading state
    
    * data fixes
    
    * lint needed
    
    * update sdk package
    
    * fix yarn.lock and add od-eth pool
    
    * cleanup
    
    * fix lint issues
    
    * format apy in poolblock
    
    * format apy on details page
    
    * deposit data
    
    * loader in the center
    
    * add link
    
    * link to blog post
    
    ---------
    
    Co-authored-by: Patrick Gallagher <blockchainbuddha@gmail.com>
    Co-authored-by: Sero <69639595+Seroxdesign@users.noreply.github.com>

--- src/containers/Earn/index.tsx
+++ src/containers/Earn/index.tsx
@@ -1,26 +1,19 @@
 import styled from 'styled-components'
+
 import PoolBlock from './PoolBlock'
 import { useStoreState } from 'easy-peasy'
 import { useStoreActions } from 'easy-peasy'
 import { useActiveWeb3React } from '~/hooks'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import useGeb from '~/hooks/useGeb'
-import { Loader } from 'react-feather'
-
-const NITRO_POOL = '0x70b4274c3f5A855c9f6f77E314D8a87CE310d03c'
-
-const pools = [
-    {
-        poolAddress: NITRO_POOL,
-        link: `https://app.camelot.exchange/nitro/${NITRO_POOL}`,
-    },
-]
+import Loader from '~/components/Loader'
+import Button from '~/components/Button'
+import { ExternalLink } from 'react-feather'
+import { POOLS } from '~/utils'
 
 const Earn = () => {
     const geb = useGeb()
-    const [loading, setLoading] = useState(false)
     const { account } = useActiveWeb3React()
-    const [apr, setApr] = useState('0')
     // @to-do for some reason the new model is not being tracked in store type, but it is available as a function
     //  @ts-ignore
     const { nitroPoolsModel: nitroPoolsState } = useStoreState((state) => state)
@@ -29,39 +22,62 @@ const Earn = () => {
     const { nitroPools } = nitroPoolsState
 
     useEffect(() => {
-        setLoading(true)
         if (!geb) return
         async function fetchPools() {
-            for (const pool of pools) {
+            for (const pool of POOLS) {
                 try {
-                    const response = await fetch('https://api.camelot.exchange/nitros')
-                    const res = await response.json()
-                    if (res.data.nitros[pool.poolAddress]?.incentivesApr) {
-                        setApr(res.data.nitros[pool.poolAddress]?.incentivesApr)
-                    }
                     await nitroPoolsActions.fetchNitroPool({
-                        geb,
-                        poolAddress: pool.poolAddress,
                         userAddress: account ?? undefined,
+                        camelotPoolAddress: pool.camelotPoolAddress,
+                        nitroPoolAddress: pool.nitroPoolAddress,
+                        geb,
                     })
-                    setLoading(false)
                 } catch (e) {
-                    setLoading(false)
                     throw new Error(`Error fetching nitropools data ${e}`)
                 }
             }
         }
         fetchPools()
     }, [account, geb, nitroPoolsActions])
+    console.log(nitroPools)
+
+    const handleClick = () => {
+        window.open('https://discord.opendollar.com/', '_blank')
+    }
 
     return (
         <Container>
             <Title>Earn</Title>
+            <SubHeader>incentivized pools and strategies</SubHeader>
+            <Text>
+                <p>Earn additional yield by staking your LP position in Camelot Nitro.</p>
+                <p>
+                    When creating a OD-ETH position, use the "Auto" mode provided by Gamma. <br></br>See full
+                    instructions{' '}
+                    <Link href="https://www.opendollar.com/blog/the-earn-page-and-new-camelot-nitro-pool" target="_blank">here</Link>.
+                </p>
+            </Text>
             <Pools>
-                {nitroPools.length > 0 &&
-                    pools?.map((pool, i) => <PoolBlock {...pool} apy={apr} nitroPoolData={nitroPools[i]} />)}
-                {loading && <Loader />}
+                <PoolsHeader>Strategies</PoolsHeader>
+                {nitroPools.length > 0 ? (
+                    POOLS?.map((pool: any, i: number) => (
+                        <PoolBlock {...pool} nitroPoolData={nitroPools[i]} key={`${pool.nitroPoolAddress}-pool`} />
+                    ))
+                ) : (
+                    <Loader width="50px" color="#1A74EC" />
+                )}
             </Pools>
+            <BtnWrapper>
+                <Button
+                    data-test-id="steps-btn"
+                    id={'suggest-pool-btn'}
+                    // text={'suggest a new pool'}
+                    secondary
+                    onClick={handleClick}
+                >
+                    suggest a new pool <ExternalLink />
+                </Button>
+            </BtnWrapper>
         </Container>
     )
 }
@@ -73,6 +89,7 @@ const Container = styled.div`
     @media (max-width: 767px) {
         margin: 50px auto;
     }
+    color: ${(props) => props.theme.colors.accent};
 `
 
 const Title = styled.h2`
@@ -80,11 +97,58 @@ const Title = styled.h2`
     font-weight: 700;
     font-family: ${(props) => props.theme.family.headers};
 
-    margin-bottom: 30px;
-
     color: ${(props) => props.theme.colors.accent};
 `
 
+const SubHeader = styled.h3`
+    text-transform: uppercase;
+    font-family: ${(props) => props.theme.family.headers};
+    font-size: 22px;
+    font-weight: 700;
+    color: ${(props) => props.theme.colors.tertiary};
+    margin-bottom: 20px;
+`
+
+const Text = styled.div`
+    background-color: #6396ff26;
+    padding: 20px;
+    font-size: ${(props) => props.theme.font.default};
+    border-radius: 3px;
+
+    p {
+        margin-bottom: 10px;
+    }
+
+    a {
+        text-decoration: underline;
+        color: ${(props) => props.theme.colors.tertiary};
+    }
+    margin-bottom: 30px;
+`
+const PoolsHeader = styled.h2`
+    font-size: 34px;
+    font-weight: 700;
+    color: ${(props) => props.theme.colors.accent};
+    margin-bottom: 20px;
+`
 const Pools = styled.div``
+
+const BtnWrapper = styled.div`
+    width: max-content;
+    margin-right: auto;
+    margin-left: auto;
+    button {
+        text-transform: uppercase;
+        font-weight: 700;
+        font-size: 18px;
+        padding: 17px 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+    }
+`
+
+const Link = styled.a``
 
 export default Earn

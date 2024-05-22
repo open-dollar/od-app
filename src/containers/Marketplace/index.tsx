6864cf5f9caced3be559ed4369866dc16afe31f5
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-05-22 11:51:36 -0400

    listings data

--- src/containers/Marketplace/index.tsx
+++ src/containers/Marketplace/index.tsx
@@ -1,36 +1,9 @@
-import { useEffect, useState } from "react"
-import { getCollectionListingsData } from "~/services/opensea"
-import { useStoreState } from "easy-peasy"
+import { useOpenSeaListings } from '~/hooks/useOpenSeaListings'
 
 const Marketplace = () => {
-  const [listings, setListings] = useState()
-
-  //@ts-ignore
-  const { safeModel: safeState } = useStoreState((state) => state)
-  const safes = safeState.list
-
-  useEffect(() => {
-    if (!safes) return;
-    const getListingData = async () => {
-      const collectionListings = await getCollectionListingsData();
-      console.log(collectionListings)
-      const listingData = collectionListings.listings.map((listing: any, i: number) => {
-        const id = listing.protocol_data.parameters.offer[0].identifierOrCriteria;
-        const safe = safes.find((safe: any) => safe.id === id)
-        const startTime = listing.protocol_data.parameters.startTime
-        const endTime = listing.protocol_data.parameters.endTime
-        return { id, safe, startTime, endTime }
-      })
-      setListings(listingData)
-    }
-    getListingData()
-  }, [safes])
-  console.log(listings)
-  return (
-    <div>
-
-    </div>
-  )
+    const listings = useOpenSeaListings()
+    console.log(listings)
+    return <div>Open Sea Listings</div>
 }
 
 export default Marketplace
--- src/services/opensea.ts
+++ src/services/opensea.ts
@@ -1,36 +1,39 @@
-
 const collectionId = 'open-dollar-vaults'
 
 const fetchFromOpenSea = async (url: string) => {
-  const options = {
-    method: 'GET',
-    headers: {
-      'accept': 'application/json',
-      'x-api-key': process.env.REACT_APP_OPENSEA_KEY!,
-    },
-  };
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            'x-api-key': process.env.REACT_APP_OPENSEA_KEY!,
+        },
+    }
 
-  return fetch(url, options)
-    .then(response => response.json())
-    .catch(err => console.error(err));
-};
+    return fetch(url, options)
+        .then((response) => response.json())
+        .catch((err) => console.error(err))
+}
 
 export const getCollectionListingsData = async () => {
-  const listings = await fetchFromOpenSea(`https://api.opensea.io/api/v2/listings/collection/${collectionId}/all`);
-  return listings;
-};
+    const listings = await fetchFromOpenSea(`https://api.opensea.io/api/v2/listings/collection/${collectionId}/all`)
+    return listings
+}
 
 export const getListingOffers = async () => {
-  const offers = await fetchFromOpenSea(`https://api.opensea.io/api/v2/offers/collection/${collectionId}/all`);
-  return offers;
-};
+    const offers = await fetchFromOpenSea(`https://api.opensea.io/api/v2/offers/collection/${collectionId}/all`)
+    return offers
+}
 
 export const getBestOfferByNFT = async (nftId: string) => {
-  const bestOffer = await fetchFromOpenSea(`https://api.opensea.io/api/v2/offers/collection/${collectionId}/nfts/${nftId}/best`);
-  return bestOffer;
-};
+    const bestOffer = await fetchFromOpenSea(
+        `https://api.opensea.io/api/v2/offers/collection/${collectionId}/nfts/${nftId}/best`
+    )
+    return bestOffer
+}
 
 export const getBestListingsByCollection = async () => {
-  const bestListings = await fetchFromOpenSea(`https://api.opensea.io/api/v2/listings/collection/${collectionId}/best`);
-  return bestListings;
-};
+    const bestListings = await fetchFromOpenSea(
+        `https://api.opensea.io/api/v2/listings/collection/${collectionId}/best`
+    )
+    return bestListings
+}

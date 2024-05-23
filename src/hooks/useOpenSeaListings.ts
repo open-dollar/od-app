7e14a106fa0d180438eb8dcb4cb2e6af9e0e41a9
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-05-23 11:24:06 -0400

    all data

--- src/hooks/useOpenSeaListings.ts
+++ src/hooks/useOpenSeaListings.ts
@@ -11,16 +11,31 @@ export const useOpenSeaListings = () => {
 
     useEffect(() => {
         if (!safes) return
+
         const getListingData = async () => {
             const collectionListings = await getCollectionListingsData()
-            const listingData = collectionListings.listings.map((listing: any, i: number) => {
+            const listingData = collectionListings.listings.map((listing: any) => {
                 const collectionAddress = listing.protocol_data.parameters.offer[0].token
-                const id = listing.protocol_data.parameters.offer[0].identifierOrCriteria
-                const safe = safes.find((safe: any) => safe.id === id)
+                const safeId = listing.protocol_data.parameters.offer[0].identifierOrCriteria
+                const owner = listing.protocol_data.parameters.offerer
+                const { id, collateral, debt, safeHandler } = safes.find((safe: any) => safe.id === safeId)
+                const totalDollarValue = +collateral - +debt
                 const startTime = listing.protocol_data.parameters.startTime
                 const endTime = listing.protocol_data.parameters.endTime
                 const price = listing.price
-                return { id, safe, startTime, endTime, collectionAddress, price }
+                return {
+                    id,
+                    startTime,
+                    endTime,
+                    collectionAddress,
+                    price,
+                    totalDollarValue,
+                    safeHandler,
+                    owner,
+                    safeId,
+                    collateral,
+                    debt,
+                }
             })
             setListings(listingData)
         }

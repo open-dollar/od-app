290ffbf2cf09632809292c3eee640746fffebd2f
Sero <69639595+Seroxdesign@users.noreply.github.com>
2024-05-23 11:56:03 -0400

    fix array

--- src/hooks/useOpenSeaListings.ts
+++ src/hooks/useOpenSeaListings.ts
@@ -11,15 +11,16 @@ export const useOpenSeaListings = () => {
     const safes = safeState.list
 
     useEffect(() => {
-        if (!safes || !account) return
+        if (!safes.length || !account) return
 
         const getListingData = async () => {
             const collectionListings = await getCollectionListingsData()
-            const listingData = collectionListings.listings.map((listing: any) => {
+            
+            const listingData = collectionListings?.listings?.map((listing: any) => {
                 const collectionAddress = listing.protocol_data.parameters.offer[0].token
                 const safeId = listing.protocol_data.parameters.offer[0].identifierOrCriteria
                 const owner = listing.protocol_data.parameters.offerer
-                const { id, collateral, debt, safeHandler } = safes.find((safe: any) => safe.id === safeId)
+                const { id, collateral, debt, safeHandler } = safes?.find((safe: any) => safe?.id === safeId)
                 const totalDollarValue = +collateral - +debt
                 const startTime = listing.protocol_data.parameters.startTime
                 const endTime = listing.protocol_data.parameters.endTime

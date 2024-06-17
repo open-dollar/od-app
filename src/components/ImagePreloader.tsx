6bd277d5975f16a57e3bbd186a8ddc819ded4d6e
Iana <36742189+alekseevaiana@users.noreply.github.com>
2024-06-17 10:26:38 -0700

    Remove unused images (#576)
    * rm imgs a-g
    
    * rm rest imgs
    
    * rm some preload images

--- src/components/ImagePreloader.tsx
+++ src/components/ImagePreloader.tsx
@@ -3,15 +3,12 @@ import odFullLogoLight from '../assets/od-full-logo-light.svg'
 import darkArrow from '../assets/dark-arrow.svg'
 import walletConnectIcon from '../assets/connectors/walletConnectIcon.svg'
 import coinbaseWalletIcon from '../assets/connectors/coinbaseWalletIcon.svg'
-import cookie from '../assets/cookie.svg'
-import caret from '../assets/caret.webp'
 import caretUp from '../assets/caret-up.svg'
 import arrowUp from '../assets/arrow-up.svg'
 import odLogo from '../assets/od-logo.svg'
 import arrow from '../assets/arrow.svg'
 import uniswapIcon from '../assets/uniswap-icon.svg'
 import metamask from '../assets/connectors/metamask.webp'
-import odColorLoop from '../assets/od-colorloop.webp'
 import closedVault from '../assets/closed-vault.webp'
 
 const INITIAL_STATE = [
@@ -20,15 +17,12 @@ const INITIAL_STATE = [
     darkArrow,
     walletConnectIcon,
     coinbaseWalletIcon,
-    cookie,
-    caret,
     caretUp,
     arrowUp,
     odLogo,
     arrow,
     uniswapIcon,
     metamask,
-    odColorLoop,
     closedVault,
 ]
 

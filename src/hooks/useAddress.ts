0f29a226221b5bd324de2b4b75512ff394647a39
Jacob Habib <47253537+jahabeebs@users.noreply.github.com>
2024-06-20 12:06:44 -0500

    Issue #512: ENS for addresses (#579)
    * useAddress hook
    
    * isMounted issue
    
    * update rpcs to be constants, add to env example
    
    * fix in right place

---
+++ src/hooks/useAddress.ts
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import { useWeb3React } from '@web3-react/core'
+import { JsonRpcProvider } from '@ethersproject/providers'
+import { RPC_URL_ETHEREUM } from '~/chains'
+
+/**
+ * Return the wallet address or ENS name if applicable
+ * @param walletAddress
+ * @param startingIndex
+ */
+export function useAddress(walletAddress: string | undefined, startingIndex: number = 0) {
+    const { provider } = useWeb3React()
+    const [address, setAddress] = useState<string | undefined>('')
+
+    const rpcProvider = new JsonRpcProvider(RPC_URL_ETHEREUM, 1)
+
+    useEffect(() => {
+        let isMounted = true
+
+        const fetchData = async () => {
+            if (walletAddress) {
+                try {
+                    const ensName = await rpcProvider.lookupAddress(walletAddress)
+                    if (isMounted) {
+                        setAddress(
+                            ensName || `${walletAddress.slice(startingIndex, 4 + 2)}...${walletAddress.slice(-4)}`
+                        )
+                    }
+                } catch (error) {
+                    if (isMounted) {
+                        setAddress(`${walletAddress.slice(startingIndex, 4 + 2)}...${walletAddress.slice(-4)}`)
+                    }
+                }
+            }
+        }
+
+        fetchData()
+
+        return () => {
+            isMounted = false
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [provider, walletAddress, startingIndex])
+
+    return address
+}

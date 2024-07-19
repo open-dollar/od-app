d49fcb945c688c82fbd3bdb9f9e3416fd234f04e
jahabeebs <jahgoreds@gmail.com>
2024-07-19 16:53:33 -0500

    fix loading logic

--- src/containers/Vaults/index.tsx
+++ src/containers/Vaults/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { useStoreState } from '~/store'
 import { useNavigate, useParams } from 'react-router-dom'
@@ -8,6 +8,8 @@ import { COIN_TICKER } from '~/utils'
 import { useTranslation } from 'react-i18next'
 import Accounts from './Accounts'
 import Loader from '~/components/Loader'
+import useGeb from '~/hooks/useGeb'
+import { useWeb3React } from '@web3-react/core'
 
 interface OnBoardingProps {
     className?: string
@@ -17,14 +19,26 @@ const OnBoarding = ({ className }: OnBoardingProps) => {
     const navigate = useNavigate()
     const { t } = useTranslation()
 
-    const [loading] = useState(false)
+    const [loading, setLoading] = useState(true)
     const { address } = useParams<{ address: string }>()
     const { connectWalletModel: connectWalletState, safeModel: safeState } = useStoreState((state) => state)
     const { isWrongNetwork, isStepLoading } = connectWalletState
+    const geb = useGeb()
+    const { account } = useWeb3React()
 
     const handleCreateSafe = () => {
         navigate('/vaults/create')
     }
+
+    useEffect(() => {
+        if (geb && !account) {
+            setLoading(false)
+        } else if ((connectWalletState.tokensData && account) || geb) {
+            setTimeout(() => {
+                setLoading(false)
+            }, 1000)
+        }
+    }, [loading, geb, connectWalletState.tokensData, account])
 
     return (
         <Container id="app-page" className={className}>
@@ -40,10 +54,10 @@ const OnBoarding = ({ className }: OnBoardingProps) => {
                             btnText={'create_safe'}
                             handleClick={handleCreateSafe}
                             isDisabled={isWrongNetwork}
-                            isLoading={isStepLoading}
+                            isStepLoading={isStepLoading}
                         />
                     </>
-                ) : loading ? (
+                ) : loading || isStepLoading ? (
                     <LoaderWrapper>
                         <Loader width="200px" color="#1A74EC" />
                     </LoaderWrapper>
